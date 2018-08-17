02d2bfd4c5b97f3a1920a577f05966e8ec29de2d
InnovativeInventor <theinnovativeinventor@gmail.com>
2018-08-17 14:30:26 -0400

    Added more tests

--- tests/goss_test.sh
+++ tests/goss_test.sh
@@ -2,6 +2,6 @@ python3 setup.py install
 git clone https://github.com/InnovativeInventor/monobox-example
 cp tests/goss.yaml monobox-example/goss.yaml
 cd monobox-example
-goss verify || ((i++))
+goss validate || ((i++))
 echo "$i"
 exit $i
